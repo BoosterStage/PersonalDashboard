5db11d3ee343accf82db039bc09d5ac8003a97f0
alessandrostein <alessandro@alessandrostein.com>
2019-02-28 14:58:14 -0300

    Goals specs

--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -3,7 +3,20 @@
 require 'test_helper'
 
 class GoalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid goal" do
+    goal = Goal.new(name: "20 Push-ups per day", benchmark: 20)
+    assert goal.valid?
+  end
+
+  test "invalid without name" do
+    goal = Goal.new(benchmark: 20)
+    refute goal.valid?
+    assert_not_nil goal.errors[:name]
+  end
+
+  test "invalid without benchmark" do
+    goal = Goal.new(name: "20 Push-ups per day")
+    refute goal.valid?
+    assert_not_nil goal.errors[:benchmark]
+  end
 end
