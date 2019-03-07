b6ca23bb495fa55225e0af380049a517f4bc812e
concept47 <ike@concept47.com>
2019-03-07 03:32:48 -0500

    Modify goals controller and views to accommodate new attribute

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -71,6 +71,6 @@ class GoalsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def goal_params
-    params.require(:goal).permit(:name)
+    params.require(:goal).permit(:name, :benchmark)
   end
 end
--- app/views/goals/_goal.json.jbuilder
+++ app/views/goals/_goal.json.jbuilder
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-json.extract! goal, :id, :name, :created_at, :updated_at
+json.extract! goal, :id, :name, :benchmark, :created_at, :updated_at
 json.url goal_url(goal, format: :json)
